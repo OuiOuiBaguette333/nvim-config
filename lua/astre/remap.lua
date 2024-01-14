c6b58cf2b9d6f128deed9a11c14443bba4cb47d2
OuiOuiBaguette333 <148035758+OuiOuiBaguette333@users.noreply.github.com>
2024-01-14 18:37:04 +0800

    Update remap.lua

--- lua/astre/remap.lua
+++ lua/astre/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<s-tab>", "<c-w>W")
 vim.keymap.set("n", "<c-b>", ":b#<cr>")
 vim.keymap.set("n", "yyy", "gg\"+yG")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+vim.keymap.set("n", "<leader>p", ":CompetiTest receive problem<cr>")
+vim.keymap.set("n", "pp", ":CompetiTest run<cr>")
 
 vim.keymap.set({'n', 't'}, "<c-\\>", "<cmd>ToggleTerm direction=float<cr>")
 
@@ -13,15 +15,37 @@ vim.keymap.set("n", "<leader>w", ":UndotreeToggle<cr>")
 vim.api.nvim_create_autocmd("FileType", {
         pattern = {'cpp'},
         callback = function(args)
-                vim.keymap.set("n", "<leader>c", ":up<cr>:!g++ -O2 -std=gnu++20 -DDEBUG -Wall -Wextra -Wshadow \"%\" -o \"%:r\"<cr>", { buffer = args.buf })
+                vim.keymap.set("n", "<leader>c", ":up<cr>:!g++ -O2 -std=gnu++20 -DDEBUG -DLOCAL -Wall -Wextra -Wshadow \"%\" -o \"%:r\"<cr>", { buffer = args.buf })
         end
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-        pattern = {'cpp'},
-        callback = function(args)
-                vim.keymap.set("n", "<leader><leader>", ":vsplit<cr><c-w>w:term \"./%:r\"<cr>", { buffer = args.buf })
-        end
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--         pattern = {'cpp'},
+--         callback = function(args)
+--                 vim.keymap.set("n", "<leader><leader>", ":vsplit<cr><c-w>w:term \"./%:r\"<cr>", { buffer = args.buf })
+--         end
+-- })
+--experiment
+vim.api.nvim_exec([[
+augroup FileTypeCPP
+        autocmd!
+        autocmd FileType cpp nnoremap <buffer> <leader><leader> :term ./<C-r>=expand('%:r')<CR><CR>
+augroup END
+]], true)
 
+
+vim.api.nvim_set_keymap(
+        "n",
+        "<space>fb",
+        ":Telescope file_browser<CR>",
+        { noremap = true }
+)
+
+-- open file_browser with the path of the current buffer
+vim.api.nvim_set_keymap(
+        "n",
+        "<space>fb",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { noremap = true }
+)
 vim.keymap.set("n", "<leader>m", ":!sudo make<cr>")
