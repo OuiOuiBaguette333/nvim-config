7cf6b13f6589cec801f41f2e7d3e464fb4151291
OuiOuiBaguette333 <148035758+OuiOuiBaguette333@users.noreply.github.com>
2024-01-23 12:06:22 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -17,18 +17,18 @@ vim.g.mapleader = " "
 vim.api.nvim_exec([[
         augroup SetCppStartingLine
                 autocmd!
-                autocmd BufRead,BufNewFile *.cpp normal! 256G9|
+                autocmd BufRead,BufNewFile *.cpp normal! 75G9|
         augroup END
 ]], false)
 
 vim.api.nvim_exec([[
         augroup SetCcStartingLine
                 autocmd!
-                autocmd BufRead,BufNewFile *.cc normal! 40G9|
+                autocmd BufRead,BufNewFile *.cc normal! 8G9|
         augroup END
 ]], false)
 local function hello()
-  return [[樱]]
+  return [[影]]
 end
 
 local function h2()
@@ -36,15 +36,11 @@ local function h2()
 end
 
 require("lazy").setup({
-        {
-                "folke/tokyonight.nvim",
-                lazy = false,
-                priority = 1000,
-                config = function()
-                        vim.cmd([[colorscheme tokyonight-night]])
-                end,
-        },
 
+	{
+		"lycuid/vim-far",
+
+	},
 
         {
                 "hoob3rt/lualine.nvim",
@@ -54,7 +50,7 @@ require("lazy").setup({
                         require('lualine').setup {
                                 options = {
                                         icons_enabled = true,
-                                        theme = 'ayu_dark',
+                                        theme = 'powerline_dark',
                                         component_separators = { left = '', right = ''},
                                         section_separators = { left = '', right = ''},
                                         disabled_filetypes = {
@@ -113,6 +109,7 @@ require("lazy").setup({
         'eandrju/cellular-automaton.nvim',
         'xiyaowong/transparent.nvim',
 
+
         {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
         {'neovim/nvim-lspconfig'},
         {'hrsh7th/cmp-nvim-lsp'},
@@ -169,42 +166,9 @@ require('competitest').setup({
                 }
         }
 })
+vim.opt.termguicolors = true
+vim.cmd("colorscheme far")
 
-require("tokyonight").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        light_style = "day", -- The theme is used when the background is set to light
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-                -- Style to be applied to different syntax groups
-                -- Value is any valid attr-list value for `:help nvim_set_hl`
-                comments = { italic = true },
-                keywords = { italic = true },
-                functions = {},
-                variables = {},
-                -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "dark", -- style for sidebars, see below
-                floats = "dark", -- style for floating windows
-        },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-})
 require("astre")
 
 
