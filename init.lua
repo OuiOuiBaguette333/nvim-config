768ba493deb6f73f3e42f3bbd142c7768db4d87e
OuiOuiBaguette333 <148035758+OuiOuiBaguette333@users.noreply.github.com>
2024-08-28 20:03:40 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -15,33 +15,47 @@ vim.cmd("au BufNewFile *.cc 0r ~/.config/nvim/tcc.cc")
 vim.g.mapleader = " "
 -- Set starting line for C++ files
 vim.api.nvim_exec([[
-        augroup SetCppStartingLine
-                autocmd!
-                autocmd BufRead,BufNewFile *.cpp normal! 75G9|
-        augroup END
+augroup SetCppStartingLine
+autocmd!
+autocmd BufRead,BufNewFile *.cpp normal! 16G5|
+augroup END
 ]], false)
 
 vim.api.nvim_exec([[
-        augroup SetCcStartingLine
-                autocmd!
-                autocmd BufRead,BufNewFile *.cc normal! 8G9|
-        augroup END
+augroup SetCcStartingLine
+autocmd!
+autocmd BufRead,BufNewFile *.cc normal! 20G5|
+augroup END
 ]], false)
 local function hello()
-  return [[影]]
+        return [[影]]
 end
 
 local function h2()
         return [[Oui Oui Oui Oui Oui Oui Oui Oui]]
 end
-
+local function h3()
+        return [[luv louis king]]
+end
 require("lazy").setup({
-
-	{
-		"lycuid/vim-far",
-
-	},
-
+        -- { 
+        --         "loctvl842/monokai-pro.nvim",
+        --         lazy = false,
+        --         priority = 1000,
+        --         config = function()
+        --                 vim.cmd([[colorscheme monokai-pro]])
+        --         end,
+        -- },
+        {
+            "tiagovla/tokyodark.nvim",
+            opts = {
+                -- custom options here
+            },
+            config = function(_, opts)
+                require("tokyodark").setup(opts) -- calling setup is optional
+                vim.cmd [[colorscheme tokyodark]]
+            end,
+        },
         {
                 "hoob3rt/lualine.nvim",
                 dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -68,7 +82,7 @@ require("lazy").setup({
                                 },
                                 sections = {
                                         lualine_a = {'mode'},
-                                        lualine_b = {'branch', 'diff', 'diagnostics'},
+                                        lualine_b = {'branch', 'diagnosis'},
                                         lualine_c = {'filename'},
                                         lualine_x = {h2, hello, 'filetype'},
                                         lualine_y = {'progress'},
@@ -94,7 +108,7 @@ require("lazy").setup({
                 dependencies = 'MunifTanjim/nui.nvim',
                 config = function() require('competitest').setup() end,
         },
-
+        --'romgrk/barbar.nvim',
         "nvim-treesitter/nvim-treesitter",
         'nvim-tree/nvim-tree.lua',
         'nvim-tree/nvim-web-devicons',
@@ -119,24 +133,54 @@ require("lazy").setup({
         {'ggandor/lightspeed.nvim'},
         {'nvim-telescope/telescope.nvim'},
         {'nvim-lua/plenary.nvim'},
+
+        -- debugger
+        {'mfussenegger/nvim-dap'},
+
         {
                 "nvim-telescope/telescope-file-browser.nvim",
                 dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
         },
+
+        { "mistricky/codesnap.nvim", build = "make" },
         {
-                'willothy/veil.nvim',
-                lazy = false,
-                priority = 500,
-                dependencies = {
-                        -- All optional, only required for the default setup.
-                        -- If you customize your config, these aren't necessary.
-                        "nvim-telescope/telescope.nvim",
-                        "nvim-lua/plenary.nvim",
-                        "nvim-telescope/telescope-file-browser.nvim"
-                },
-                config = true,
-                -- or configure with:
-                -- opts = { ... }
+                'nvimdev/dashboard-nvim',
+                event = 'VimEnter',
+                config = function()
+                        require('dashboard').setup {
+                                theme = 'hyper',
+                                config = {
+                                        week_header = {
+                                                enable = true,
+                                        },
+                                        shortcut = {
+                                                { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                                                {
+                                                        icon = ' ',
+                                                        icon_hl = '@variable',
+                                                        desc = 'Files',
+                                                        group = 'Label',
+                                                        action = 'Telescope find_files',
+                                                        key = 'f',
+                                                },
+                                                {
+                                                        desc = ' Apps',
+                                                        group = 'DiagnosticHint',
+                                                        action = 'Telescope app',
+                                                        key = 'a',
+                                                },
+                                                {
+                                                        desc = ' dotfiles',
+                                                        group = 'Number',
+                                                        action = 'Telescope dotfiles',
+                                                        key = 'd',
+                                                },
+                                        },
+                                        packages = { enabled = true },
+                                }, 
+                        }
+                end,
+                dependencies = { {'nvim-tree/nvim-web-devicons'}}
         },
 })
 
@@ -146,7 +190,7 @@ require("telescope").load_extension "file_browser"
 require('competitest').setup({
         template_file = "~/.config/nvim/template.cpp",
         compile_command = {
-                cpp = { exec = "g++", args = { "-O2", "-std=gnu++20",  "-DDEBUG", "-Wall", "-Wextra", "-Wshadow", "$(FNAME)", "-o", "$(FNOEXT)" } },
+                cpp = { exec = "g++", args = { "-O2", "-std=gnu++20",  "-DDEBUG", "-DLOCAL", "-Wall", "-Wextra", "-Wshadow", "$(FNAME)", "-o", "$(FNOEXT)" } },
         },
         popup_ui = {
                 total_width = 0.8,
@@ -166,9 +210,38 @@ require('competitest').setup({
                 }
         }
 })
-vim.opt.termguicolors = true
-vim.cmd("colorscheme far")
+local dap = require('dap')
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
 
+    -- 💀
+    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --
+    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --
+    -- Otherwise you might get the following error:
+    --
+    --    Error on launch: Failed to attach to the target process
+    --
+    -- But you should be aware of the implications:
+    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    -- runInTerminal = false,
+  },
+}
+require("codesnap").setup({
+        watermark = "OuiOuiBaguette"
+})
+-- vim.opt.termguicolors = true
+-- vim.cmd("colorscheme far")
 require("astre")
 
 
